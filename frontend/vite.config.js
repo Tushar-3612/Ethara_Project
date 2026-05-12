9c11876cba0dee125071172808ad2b8d972eeb4a
Tushar-3612 <tusharnagare2006@gmail.com>
2026-05-12 21:08:30 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 8080,
+    allowedHosts: ['awake-magic-production-3f5a.up.railway.app']
+  }
 })
